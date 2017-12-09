ea7dca93b8c4e4f39f08f588bf954f0cd3565b6b
galakerman <akermang@gmail.com>
2017-12-09 13:21:41 +0200

    global path

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, './assets')));
 
 app.use('/api', api);
 app.use(express.static(path.join(__dirname, 'dist')));
