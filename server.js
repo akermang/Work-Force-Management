70e250b821627bf845c434f14ab87331950fb8f3
galakerman <akermang@gmail.com>
2017-12-09 13:16:57 +0200

    global path

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(fileUpload());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api', api);
-
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
